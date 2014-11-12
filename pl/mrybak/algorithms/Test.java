7604c21a616cf3f3f94a3a64b6a23fe7a59f6081
Michał Rybak <kontakt@mrybak.pl>
2014-11-12 14:51:00 +0100

    cleanup

--- pl/mrybak/algorithms/Test.java
+++ pl/mrybak/algorithms/Test.java
@@ -1,7 +1,6 @@
 package pl.mrybak.algorithms;
 
 import java.util.Arrays;
-import pl.mrybak.datastructures.UndirectedGraph;
 
 public class Test {
   public static void main(String[] args) {
@@ -16,36 +15,6 @@ public class Test {
     System.out.println("binsearch for 12: " + Binsearch.search(sorted, 12)); 
     System.out.println("binsearch for 0: " + Binsearch.search(sorted, 0)); 
     System.out.println("binsearch for 6: " + Binsearch.search(sorted, 6));
-    System.out.println("binsearch for 7 (missing): " + Binsearch.search(sorted, 7));
-        
-    // TODO: move to datastructures.test
-    UndirectedGraph<Integer> g = new UndirectedGraph<>();
-    g.addNode(1, 10);
-    g.addNode(2, 20);
-    g.addNode(3, 30);
-    g.addNode(4, 40);
-    g.addNode(5, 50);
-    g.addNode(6, 60);
-    g.addNode(7, 70);
-    
-    g.addEdge(1,3);
-    g.addEdge(2,3);
-    g.addEdge(6,3);    
-    g.addEdge(2,4);    
-    g.addEdge(2,5); // comment out this lined to turn g into 2-tree forest
-    g.addEdge(7,5);    
-    g.addEdge(4,1);
-    
-    // will throw exception
-    // g.addEdge(0,1);  
-    
-    
-    System.out.println(g);
-    
-    System.out.println("DFS traversal: ");
-    g.traverseDFS();    
-    
-    System.out.println("\nBFS traversal: ");
-    g.traverseBFS();
+    System.out.println("binsearch for 7 (missing): " + Binsearch.search(sorted, 7));      
   }
 }
--- pl/mrybak/datastructures/Test.java
+++ pl/mrybak/datastructures/Test.java
@@ -2,7 +2,10 @@ package pl.mrybak.datastructures;
 
 public class Test {
   public static void main(String[] args) {
-    // basic linked list operations
+    
+    /**
+      * LinkedList tests
+      */
     SinglyLinkedList<Integer> l1 = new SinglyLinkedList<>();
   
     l1.append(2);
@@ -66,6 +69,38 @@ public class Test {
     bst.delete(2);
     System.out.println("=== BFS ===");
     bst.enumerateBFS();
+    System.out.println();
     
+    /**
+      * Graphs tests
+      */
+    UndirectedGraph<Integer> g = new UndirectedGraph<>();
+    g.addNode(1, 10);
+    g.addNode(2, 20);
+    g.addNode(3, 30);
+    g.addNode(4, 40);
+    g.addNode(5, 50);
+    g.addNode(6, 60);
+    g.addNode(7, 70);
+    
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(6,3);    
+    g.addEdge(2,4);    
+    g.addEdge(2,5); // comment out this lined to turn g into 2-tree forest
+    g.addEdge(7,5);    
+    g.addEdge(4,1);
+    
+    // will throw exception
+    // g.addEdge(0,1);  
+    
+    
+    System.out.println(g);
+    
+    System.out.println("DFS traversal: ");
+    g.traverseDFS();    
+    
+    System.out.println("\nBFS traversal: ");
+    g.traverseBFS();
   }
 }
