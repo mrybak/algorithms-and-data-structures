927f834f62350a87f336393285f708f0c8f767c1
Michał Rybak <kontakt@mrybak.pl>
2015-01-13 19:25:44 +0100

    subarray with maximum sum

--- pl/mrybak/algorithms/MatrixAlgs.java
+++ pl/mrybak/algorithms/MatrixAlgs.java
@@ -1,5 +1,10 @@
 package pl.mrybak.algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class MatrixAlgs {
     public static int maxPath(int[][] m) {
         // List<String> result = new ArrayList<>();
@@ -40,5 +45,69 @@ public class MatrixAlgs {
         }
 
         return maxSumSoFar;
+    }
+
+    /**
+     * Return subarray with maximum sum;
+     * We assume matrix is nonempty and no column and no row consists of negative integers only
+     *
+     * TODO: comments & clarification 
+     */
+    public static int[][] maxSumSubarray(int[][] m) {
+        int rows = m.length;
+        int cols = m[0].length;
+
+        int[][] maximums = new int[cols][cols];
+        int[][] topRow = new int[cols][cols];
+        int[][] bottomRow = new int[cols][cols];
+
+
+        for (int left = 0; left < cols; left++) {
+            int[] rowSum = new int[rows];
+            for (int right = left; right < cols; right++) {
+                // for every possible left-right column combination, compute row sum for every row:
+                for (int k = 0; k < rows; k++) {
+                     rowSum[k] += m[k][right];
+                }
+                // and apply kadane's algorithm to it, saving top/bottom row index in the meantime:
+                int maxSumEndingHere = 0, maxSumSoFar = 0;
+
+                for (int i = 0; i < rowSum.length; i++) {
+                    int elem = rowSum[i];
+                    if (maxSumEndingHere + elem < 0) {
+                        topRow[left][right] = i+1;
+                    }
+                    maxSumEndingHere = Math.max(maxSumEndingHere + elem, 0);
+                    if (maxSumEndingHere > maxSumSoFar) {
+                        bottomRow[left][right] = i;
+                        maxSumSoFar = maxSumEndingHere;
+                    }
+                }
+                maximums[left][right] = maxSumSoFar;
+            }
+        }
+
+        int maxSum = 0, fromX = 0, fromY = 0, toX = 0, toY = 0;
+        for (int i = 0; i < cols; i++) {
+            for (int j = i; j < cols; j++) {
+                int sum = maximums[i][j];
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    fromX = i;
+                    fromY = topRow[i][j];
+                    toX = j;
+                    toY = bottomRow[i][j];
+                }
+            }
+        }
+
+        int[][] result = new int[toY - fromY + 1][toX - fromX + 1];
+        for (int i = fromY; i <= toY ; i++) {
+            for (int j = fromX; j < toX; j++) {
+                result[i-fromY][j-fromX] = m[i][j];
+            }
+        }
+
+        return result;
     }
 }
--- pl/mrybak/algorithms/TestMatrixAlgs.java
+++ pl/mrybak/algorithms/TestMatrixAlgs.java
@@ -1,5 +1,6 @@
 package pl.mrybak.algorithms;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,5 +19,15 @@ public class TestMatrixAlgs {
         ));
 
         System.out.println(MatrixAlgs.maxContinuousSum(new int[]{-2,-3,3,4,1,-4,5,-2,0,0,-7,3,8}));
+
+        int[][] subArr = MatrixAlgs.maxSumSubarray(new int[][] {
+                { 6,-2, 3, 5,-7,-8, 1, 2},
+                {-5, 4,-9, 1,-4,-2,-6, 5},
+                { 4,-5, 6,-3,-7, 5, 0,-1},
+                { 1, 0,-2, 7,-4, 6, 3,-9},
+                { 5,-1, 9,-8, 7, 0, 0, 3}
+        });
+
+        System.out.println(Arrays.deepToString(subArr));
     }
 }
